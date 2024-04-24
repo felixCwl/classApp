cea8ca02fd6790bc970db47f0e69b4612bac6dde
felixChan <felix.chan@uafinance.com>
2024-04-24 10:52:39 +0800

    add docker-compose to start back/frontend

--- sample-backend/class-app-class-service/src/main/java/com/example/classAppClassService/feign/FileServiceFeign.java
+++ sample-backend/class-app-class-service/src/main/java/com/example/classAppClassService/feign/FileServiceFeign.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 
-@FeignClient(name = "CLASS-APP-FILE-SERVICE")
+@FeignClient(name = "${file.name}")
 public interface FileServiceFeign {
     @PostMapping(value = "file-service/file-convert/getXlsxToJson", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<JsonNode> getXlsxToJson(@RequestPart("file") MultipartFile file);
